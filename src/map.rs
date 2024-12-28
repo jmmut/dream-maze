ffc99e47b5343a48265b4268452009671b1117ae
jose miguel mut <jomutlo@gmail.com>
2024-12-28 12:03:15 +0100

    add collision

--- src/map.rs
+++ src/map.rs
@@ -1,9 +1,16 @@
-use macroquad::prelude::UVec2;
+use macroquad::prelude::{IVec2, UVec2};
 use macroquad::rand::rand;
 use std::ops::IndexMut;
 
 pub type Coord = u32;
 pub type Coord2 = UVec2;
+pub type CoordDiff = i32;
+pub type CoordDiff2 = IVec2;
+
+const DOWN: CoordDiff2 = CoordDiff2::new(0, 1);
+const UP: CoordDiff2 = CoordDiff2::new(0, -1);
+const LEFT: CoordDiff2 = CoordDiff2::new(-1, 0);
+const RIGHT: CoordDiff2 = CoordDiff2::new(1, 0);
 
 #[derive(Copy, Clone, PartialEq)]
 pub enum Tile {
@@ -15,11 +22,8 @@ pub enum Tile {
 pub struct Map {
     tiles: Vec<Vec<Tile>>,
     offset: Coord2,
+    player: Coord2,
 }
-
-impl Map {}
-
-impl Map {}
 
 impl Map {
     pub fn new(screen_tiles: Coord2, player: Coord2) -> Self {
@@ -36,48 +40,77 @@ impl Map {
             tiles.push(column);
         }
         let offset = Coord2::new(0, 0);
-        let mut map = Self { tiles, offset };
-        *map.get_mut(player.x, player.y) = Tile::Floor;
+        let mut map = Self {
+            tiles,
+            offset,
+            player,
+        };
+        *map.get_mut(player) = Tile::Floor;
         map
     }
 
     pub fn is_wall(&self, x: Coord, y: Coord) -> bool {
+        self.get(Coord2::new(x, y)) == Tile::Wall
+    }
+
+    pub fn move_down(&mut self) {
+        self.move_to(DOWN);
+    }
+    pub fn move_up(&mut self) {
+        self.move_to(UP);
+    }
+    pub fn move_right(&mut self) {
+        self.move_to(RIGHT);
+    }
+    pub fn move_left(&mut self) {
+        self.move_to(LEFT);
+    }
+    pub fn move_to(&mut self, diff: CoordDiff2) {
+        if self.get_rel(self.player, diff) != Tile::Wall {
+            self.offset = self.add_coord(self.offset, diff)
+        }
+    }
+}
+impl Map {
+    fn get_raw(&self, x: Coord, y: Coord) -> Tile {
+        self.tiles[x as usize][y as usize]
+    }
+    fn get(&self, pos: Coord2) -> Tile {
         let Coord2 {
             x: size_x,
             y: size_y,
         } = self.size();
-        let tile = self.get((x + self.offset.x) % size_x, (y + self.offset.y) % size_y);
-        tile == Tile::Wall
+        self.get_raw(
+            (pos.x + self.offset.x) % size_x,
+            (pos.y + self.offset.y) % size_y,
+        )
     }
-
-    pub fn move_down(&mut self) {
-        self.offset.y += 1;
-        self.offset.y %= self.size().y;
+    fn get_rel(&self, pos: Coord2, diff: CoordDiff2) -> Tile {
+        self.get(self.add_coord(pos, diff))
     }
-    pub fn move_up(&mut self) {
-        let size_y = self.size().y;
-        self.offset.y += size_y - 1;
-        self.offset.y %= self.size().y;
-    }
-    pub fn move_right(&mut self) {
-        self.offset.x += 1;
-        self.offset.x %= self.size().x;
-    }
-    pub fn move_left(&mut self) {
-        let size_x = self.size().x;
-        self.offset.x += size_x - 1;
-        self.offset.x %= self.size().x;
-    }
-}
-impl Map {
-    fn get(&self, x: Coord, y: Coord) -> Tile {
-        self.tiles[x as usize][y as usize]
-    }
-    fn get_mut(&mut self, x: Coord, y: Coord) -> &mut Tile {
+    fn get_raw_mut(&mut self, x: Coord, y: Coord) -> &mut Tile {
         self.tiles.index_mut(x as usize).index_mut(y as usize)
+    }
+    fn get_mut(&mut self, pos: Coord2) -> &mut Tile {
+        let Coord2 {
+            x: size_x,
+            y: size_y,
+        } = self.size();
+        self.get_raw_mut(
+            (pos.x + self.offset.x) % size_x,
+            (pos.y + self.offset.y) % size_y,
+        )
     }
     fn size(&self) -> Coord2 {
         Coord2::new(self.tiles.len() as Coord, self.tiles[0].len() as Coord)
+    }
+    fn add_coord(&self, pos: Coord2, diff: CoordDiff2) -> Coord2 {
+        let size = self.size();
+        let unsigned_diff = Coord2::new(
+            (diff.x + size.x as CoordDiff) as Coord,
+            (diff.y + size.y as CoordDiff) as Coord,
+        );
+        (pos + unsigned_diff) % size
     }
 }
 
